20ecd5db63b45bb1d41837e670a8595b8fd29cda
David Johnston <davidajohnston2000@gmail.com>
2022-08-14 22:41:08 +0100

    Introduce a code smell

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,5 @@ import App from './App';
 it('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).not.toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
