1fbf2f893cdeb24626f4ef2e82b0a221cbd3da01
David Johnston <davidajohnston2000@gmail.com>
2022-08-14 22:48:10 +0100

    Introduce a code smell for sonarcloud to report on

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const foo = () => {};
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {Math.random() == 0 ? <img src={logo} className="App-logo" alt="logo" /> : <div>Dave</div>}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
