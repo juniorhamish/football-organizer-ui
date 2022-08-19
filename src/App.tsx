97d4613f63a9f4d27ab36d7cda1a2d3cfba7ae7d
David Johnston <davidajohnston2000@gmail.com>
2022-08-19 01:07:13 +0100

    Add authentication to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator signUpAttributes={['email', 'given_name', 'family_name']} socialProviders={['amazon', 'facebook', 'google']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.attributes?.given_name}</h1>
+          <button type="button" onClick={signOut}>
+            Sign out
+          </button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
---
+++ src/aws-exports.ts
@@ -0,0 +1,19 @@
+const awsConfig = {
+  Auth: {
+    region: 'eu-west-2',
+    userPoolId: 'eu-west-2_AQejk3Z18',
+    userPoolWebClientId: '5cfiuv2vlmnqu4mu1ng3dfkaar',
+    signUpVerificationMethod: 'code',
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+    mandatorySignIn: false,
+    oauth: {
+      domain: 'football-organizer.auth.eu-west-2.amazoncognito.com',
+      scope: ['phone', 'email', 'openid', 'profile', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: 'http://localhost:3000',
+      redirectSignOut: 'http://localhost:3000',
+      responseType: 'code',
+    },
+  },
+};
+
+export default awsConfig;
