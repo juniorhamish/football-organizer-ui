f2882ec4db877b54e8f5572ab23bb35f20a63d77
David Johnston <davidajohnston2000@gmail.com>
2022-08-19 01:25:55 +0100

    Add authentication to the application (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsconfig from './aws-exports';
+
+awsconfig.Auth.oauth.redirectSignIn = `${window.location.origin}`;
+awsconfig.Auth.oauth.redirectSignOut = `${window.location.origin}`;
+
+Amplify.configure(awsconfig);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator signUpAttributes={['email', 'given_name', 'family_name']} socialProviders={['amazon', 'facebook', 'google']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.attributes?.given_name}</h1>
+          <button type="button" onClick={signOut}>
+            Sign out
+          </button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
