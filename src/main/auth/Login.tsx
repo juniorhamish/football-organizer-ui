53f436ad13d360d1c30c3a36a0dc51db7f61f2d0
David Johnston <davidajohnston2000@gmail.com>
2022-09-05 12:17:38 +0100

    Show errors when login fails

--- src/main/auth/Login.tsx
+++ src/main/auth/Login.tsx
@@ -1,5 +1,5 @@
-import { Button, Card, CardActions, CardContent, CardHeader, Container, FormControl, IconButton, InputAdornment, TextField } from '@mui/material';
-import { SyntheticEvent, useRef, useState } from 'react';
+import { Backdrop, Button, Card, CardActions, CardContent, CardHeader, CircularProgress, Container, FormControl, FormHelperText, IconButton, InputAdornment, TextField } from '@mui/material';
+import { SyntheticEvent, useId, useRef, useState } from 'react';
 import { Auth } from 'aws-amplify';
 import { CognitoUser } from 'amazon-cognito-identity-js';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
@@ -13,46 +13,76 @@ const textFieldStyle = {
 export default function Login({ onLogin }: { onLogin: (user: CognitoUser) => void }) {
   const userNameRef = useRef<HTMLInputElement>();
   const passwordRef = useRef<HTMLInputElement>();
+  const baseId = useId();
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [showLoginFailedMessage, setShowLoginFailedMessage] = useState(false);
+  const [loginInProgress, setLoginInProgress] = useState(false);
+  const errorMessageFieldId = `${baseId}ErrorMessageField`;
 
   const login = (event: SyntheticEvent) => {
     event.preventDefault();
-    Auth.signIn(userNameRef.current?.value as string, passwordRef.current?.value).then((user) => {
-      onLogin(user);
-    });
+    setLoginInProgress(true);
+    Auth.signIn(userNameRef.current?.value as string, passwordRef.current?.value)
+      .then((user) => {
+        onLogin(user);
+      })
+      .catch(() => {
+        setShowLoginFailedMessage(true);
+      })
+      .finally(() => setLoginInProgress(false));
   };
   return (
-    <Container maxWidth="sm">
-      <Card raised>
-        <CardHeader title="Sign In" />
-        <form onSubmit={login} aria-label="Sign In Form">
-          <CardContent>
-            <FormControl fullWidth sx={formFieldStyle}>
-              <TextField label="Username" inputRef={userNameRef} sx={textFieldStyle} />
-            </FormControl>
-            <FormControl fullWidth sx={formFieldStyle}>
-              <TextField
-                label="Password"
-                inputRef={passwordRef}
-                type={passwordVisible ? 'text' : 'password'}
-                sx={textFieldStyle}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton aria-label={`${passwordVisible ? 'Hide' : 'Show'} Password`} onClick={() => setPasswordVisible(!passwordVisible)}>
-                        {passwordVisible ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </FormControl>
-          </CardContent>
-          <CardActions>
-            <Button type="submit">Submit</Button>
-          </CardActions>
-        </form>
-      </Card>
-    </Container>
+    <>
+      <Backdrop open={loginInProgress} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <CircularProgress aria-label="Login in progress" />
+      </Backdrop>
+      <Container maxWidth="sm">
+        <Card raised>
+          <CardHeader title="Sign In" />
+          <form onSubmit={login} aria-label="Sign In Form" aria-busy={loginInProgress}>
+            <CardContent>
+              <FormControl fullWidth sx={formFieldStyle}>
+                <TextField
+                  error={showLoginFailedMessage}
+                  inputProps={showLoginFailedMessage ? { 'aria-errormessage': errorMessageFieldId } : {}}
+                  label="Username"
+                  inputRef={userNameRef}
+                  sx={textFieldStyle}
+                  onChange={() => setShowLoginFailedMessage(false)}
+                />
+              </FormControl>
+              <FormControl fullWidth sx={formFieldStyle}>
+                <TextField
+                  label="Password"
+                  error={showLoginFailedMessage}
+                  inputRef={passwordRef}
+                  type={passwordVisible ? 'text' : 'password'}
+                  sx={textFieldStyle}
+                  onChange={() => setShowLoginFailedMessage(false)}
+                  InputProps={{
+                    inputProps: showLoginFailedMessage ? { 'aria-errormessage': errorMessageFieldId } : {},
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton aria-label={`${passwordVisible ? 'Hide' : 'Show'} Password`} onClick={() => setPasswordVisible(!passwordVisible)}>
+                          {passwordVisible ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+              {showLoginFailedMessage && (
+                <FormHelperText error id={errorMessageFieldId}>
+                  Login failed
+                </FormHelperText>
+              )}
+            </CardContent>
+            <CardActions>
+              <Button type="submit">Submit</Button>
+            </CardActions>
+          </form>
+        </Card>
+      </Container>
+    </>
   );
 }
