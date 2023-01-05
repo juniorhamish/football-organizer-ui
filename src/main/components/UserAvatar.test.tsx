fead16a95a41f20e09c69dc5a2e1fb46680b6acc
David Johnston <davidajohnston2000@gmail.com>
2023-01-05 12:48:02 +0000

    Fix linting issues in tests

--- src/main/components/UserAvatar.test.tsx
+++ src/main/components/UserAvatar.test.tsx
@@ -1,15 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import UserAvatar from './UserAvatar';
 
 describe('user avatar', () => {
   it('should show the users initials', () => {
-    const { getByText } = render(<UserAvatar name="David Johnston" />);
+    render(<UserAvatar name="David Johnston" />);
 
-    expect(getByText('DJ')).toBeInTheDocument();
+    expect(screen.getByText('DJ')).toBeInTheDocument();
   });
   it('should set the background colour based on the name', () => {
-    const { container } = render(<UserAvatar name="David Johnston" />);
+    render(<UserAvatar name="David Johnston" />);
 
-    expect(container.firstChild).toHaveStyle('background-color: #F76B23');
+    expect(screen.getByText('DJ')).toHaveStyle('background-color: #F76B23');
   });
 });
