296ee6d9cf83edf0cc9d7d32f073506480e7261a
David Johnston <davidajohnston2000@gmail.com>
2023-01-07 22:30:13 +0000

    Remove duplication of form state update

---
+++ src/main/functional/useFormState.ts
@@ -0,0 +1,18 @@
+import { ChangeEvent, useState } from 'react';
+
+const useFormState = <T>(initialState: T, beforeChange: () => void) => {
+  const [values, setValues] = useState(initialState);
+  const onChange = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    beforeChange();
+    const fieldName = event.target.name;
+    setValues((prevState) => {
+      return {
+        ...prevState,
+        [fieldName]: event.target.value,
+      };
+    });
+  };
+  return { values, onChange };
+};
+
+export default useFormState;
