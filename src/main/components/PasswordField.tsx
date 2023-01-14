cfad8fe800bf89c397593290ed6e830de5105c91
David Johnston <davidajohnston2000@gmail.com>
2023-01-14 22:10:42 +0000

    Simplify password field

--- src/main/components/PasswordField.tsx
+++ src/main/components/PasswordField.tsx
@@ -1,22 +1,10 @@
 import { IconButton, InputAdornment, OutlinedInputProps } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { ChangeEvent, useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import BoxShadowOutlinedInput from './BoxShadowOutlinedInput';
 
 export default function PasswordField(props: OutlinedInputProps) {
-  const { onChange } = props;
-  const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const passwordChangeHandler = useCallback(
-    (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-      setPassword(event.target.value);
-      if (onChange) {
-        onChange(event);
-      }
-    },
-    [setPassword, onChange]
-  );
 
   const showPasswordClickHandler = useCallback(() => {
     setPasswordVisible(!passwordVisible);
@@ -25,9 +13,7 @@ export default function PasswordField(props: OutlinedInputProps) {
   return (
     <BoxShadowOutlinedInput
       label="Password"
-      value={password}
       type={passwordVisible ? 'text' : 'password'}
-      onChange={passwordChangeHandler}
       endAdornment={
         <InputAdornment position="end">
           <IconButton aria-label={`${passwordVisible ? 'Hide' : 'Show'} Password`} onClick={showPasswordClickHandler}>
