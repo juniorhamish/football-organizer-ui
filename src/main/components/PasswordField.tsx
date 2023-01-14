508e42f590a111ae68f001798e5c54252e8aaf08
David Johnston <davidajohnston2000@gmail.com>
2023-01-14 22:32:57 +0000

    Show specific errors when logging in (#157)

--- src/main/components/PasswordField.tsx
+++ src/main/components/PasswordField.tsx
@@ -1,28 +1,10 @@
-import { IconButton, InputAdornment, InputBaseComponentProps } from '@mui/material';
+import { IconButton, InputAdornment, OutlinedInputProps } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { ChangeEvent, ChangeEventHandler, useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import BoxShadowOutlinedInput from './BoxShadowOutlinedInput';
 
-type PasswordFieldProps = {
-  id: string;
-  name?: string;
-  error?: boolean;
-  onChange: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
-  inputProps?: InputBaseComponentProps;
-  autoComplete?: string;
-};
-
-export default function PasswordField({ id, name, error, onChange, inputProps, autoComplete }: PasswordFieldProps) {
-  const [password, setPassword] = useState('');
+export default function PasswordField(props: OutlinedInputProps) {
   const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const passwordChangeHandler = useCallback(
-    (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-      setPassword(event.target.value);
-      onChange(event);
-    },
-    [setPassword, onChange]
-  );
 
   const showPasswordClickHandler = useCallback(() => {
     setPasswordVisible(!passwordVisible);
@@ -30,15 +12,8 @@ export default function PasswordField({ id, name, error, onChange, inputProps, a
 
   return (
     <BoxShadowOutlinedInput
-      id={id}
-      name={name}
       label="Password"
-      error={error}
-      value={password}
       type={passwordVisible ? 'text' : 'password'}
-      autoComplete={autoComplete}
-      onChange={passwordChangeHandler}
-      inputProps={inputProps}
       endAdornment={
         <InputAdornment position="end">
           <IconButton aria-label={`${passwordVisible ? 'Hide' : 'Show'} Password`} onClick={showPasswordClickHandler}>
@@ -46,13 +21,7 @@ export default function PasswordField({ id, name, error, onChange, inputProps, a
           </IconButton>
         </InputAdornment>
       }
+      {...props}
     />
   );
 }
-
-PasswordField.defaultProps = {
-  error: false,
-  inputProps: {},
-  autoComplete: 'password',
-  name: undefined,
-};
