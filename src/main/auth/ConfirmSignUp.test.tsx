822587ba658c170b7a57c8784d6cd3b6513d0346
David Johnston <davidajohnston2000@gmail.com>
2023-05-20 15:56:14 +0100

    Show progress indicator on signup and confirm

--- src/main/auth/ConfirmSignUp.test.tsx
+++ src/main/auth/ConfirmSignUp.test.tsx
@@ -1,10 +1,10 @@
 import { Auth } from 'aws-amplify';
-import { render, within } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import ConfirmSignUp from './ConfirmSignUp';
 import mocked = jest.mocked;
-import { codeField, confirmButton, confirmSignUp, confirmSignUpForm, resendCodeButton } from './ConfirmSignUp.test.helpers';
+import { codeField, confirmButton, confirmSignUp, confirmSignupFailedError, confirmSignUpForm, resendCodeButton } from './ConfirmSignUp.test.helpers';
 
 jest.mock('aws-amplify');
 
@@ -61,6 +61,32 @@ describe('confirm sign up', () => {
 
     expect(onConfirm).toHaveBeenCalled();
   });
+  it('should not invoke the onConfirm callback on failure', async () => {
+    mocked(Auth).confirmSignUp.mockRejectedValue({});
+    const onConfirm = jest.fn();
+    renderWithRouter('foobar', onConfirm);
+
+    await confirmSignUp('ABCD1234');
+
+    expect(onConfirm).not.toHaveBeenCalled();
+  });
+  it('should mark the code field as invalid if confirm signup fails', async () => {
+    mocked(Auth).confirmSignUp.mockRejectedValue(confirmSignupFailedError());
+    renderWithRouter();
+
+    await confirmSignUp('ABCD1234');
+
+    expect(codeField()).toHaveErrorMessage('Invalid code');
+  });
+  it('should show the code field as valid again if it is subsequently modified', async () => {
+    mocked(Auth).confirmSignUp.mockRejectedValue(confirmSignupFailedError());
+    renderWithRouter();
+    await confirmSignUp('ABCD1234');
+
+    await userEvent.type(codeField(), 'XYZ');
+
+    expect(codeField()).not.toHaveErrorMessage();
+  });
   it('should invoke the Auth resend confirmation code on button click', async () => {
     mocked(Auth).resendSignUp.mockImplementation(() => new Promise(jest.fn()));
     renderWithRouter('spakowsky');
@@ -68,5 +94,107 @@ describe('confirm sign up', () => {
     await userEvent.click(resendCodeButton());
 
     expect(Auth.resendSignUp).toHaveBeenCalledWith('spakowsky');
+  });
+  describe('progress indicator', () => {
+    describe('confirm', () => {
+      it('should show a progress mask when confirm sign up is in progress', async () => {
+        mocked(Auth).confirmSignUp.mockImplementation(() => new Promise(jest.fn()));
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm sign up in progress')).toBeVisible();
+      });
+      it('should mark the confirm sign up form as busy when confirm sign up is in progress', async () => {
+        mocked(Auth).confirmSignUp.mockImplementation(() => new Promise(jest.fn()));
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'true');
+      });
+      it('should hide the progress mask when sign up succeeds', async () => {
+        mocked(Auth).confirmSignUp.mockResolvedValue({});
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm sign up in progress')).not.toBeVisible();
+      });
+      it('should remove the busy marker from the confirm sign up form when confirm sign up succeeds', async () => {
+        mocked(Auth).confirmSignUp.mockResolvedValue({});
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'false');
+      });
+      it('should hide the progress mask when confirm sign up fails', async () => {
+        mocked(Auth).confirmSignUp.mockRejectedValue({});
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm sign up in progress')).not.toBeVisible();
+      });
+      it('should remove the busy marker from the confirm sign up form when confirm sign up fails', async () => {
+        mocked(Auth).confirmSignUp.mockRejectedValue({});
+        renderWithRouter();
+
+        await confirmSignUp('ABCD');
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'false');
+      });
+    });
+    describe('resend code', () => {
+      it('should show a progress mask when resend code is in progress', async () => {
+        mocked(Auth).resendSignUp.mockImplementation(() => new Promise(jest.fn()));
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Resend confirmation code request in progress')).toBeVisible();
+      });
+      it('should mark the confirm sign up form as busy when resend code is in progress', async () => {
+        mocked(Auth).resendSignUp.mockImplementation(() => new Promise(jest.fn()));
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'true');
+      });
+      it('should hide the progress mask when resend code succeeds', async () => {
+        mocked(Auth).resendSignUp.mockResolvedValue({});
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Resend confirmation code request in progress')).not.toBeVisible();
+      });
+      it('should remove the busy marker from the confirm sign up form when resend code succeeds', async () => {
+        mocked(Auth).resendSignUp.mockResolvedValue({});
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'false');
+      });
+      it('should hide the progress mask when resend code fails', async () => {
+        mocked(Auth).resendSignUp.mockRejectedValue({});
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Resend confirmation code request in progress')).not.toBeVisible();
+      });
+      it('should remove the busy marker from the confirm sign up form when resend code fails', async () => {
+        mocked(Auth).resendSignUp.mockRejectedValue({});
+        renderWithRouter();
+
+        await userEvent.click(resendCodeButton());
+
+        expect(screen.getByLabelText('Confirm Sign Up Form')).toHaveAttribute('aria-busy', 'false');
+      });
+    });
   });
 });
